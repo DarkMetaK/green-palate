33e2c49af254884cbcaac654c37d1e694e5449b4
DarkMetaK <matporto03@gmail.com>
2023-03-30 15:12:51 -0300

    Added css SSR

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+import { getCssText } from '@/styles/config'
+
 export default function Document() {
   return (
     <Html lang="pt-BR">
@@ -7,6 +9,8 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Tamil:wght@700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+
+        <style id='stitches' dangerouslySetInnerHTML={{__html: getCssText()}} />
       </Head>
       <body>
         <Main />
